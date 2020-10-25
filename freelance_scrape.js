01cb2f326f7cbd78952724587b397bd5e7ef7191
Firzan97 <FirzanAzrai97@gmail.com>
2020-10-26 01:54:35 +0800

    append data in array

--- freelance_scrape.js
+++ freelance_scrape.js
@@ -3,80 +3,113 @@ const puppeteer = require('puppeteer');
 // const con = require('./connection');
 
 const b = (
-    
-    async () => {
-    const browser = await puppeteer.launch ({ 
-        headless: false });
-    const page = await browser.newPage();
-    await page.goto('https://www.workana.com/en/freelancers/malaysia', { 
-        waitUntil: 'load' });
-    // await page.waitForSelector('.result_text a');
-    
-    await page.setViewport({
-        width: 1200,
-        height: 800
-    });
 
-//     const searchButtonNodeSelector = "div > div.js-pager-container > div.text-center.hidden-xs.wk-pager-scroll > nav > ul > li:nth-child(2) > a";
-// await page.click(searchButtonNodeSelector);
-var data;
-var count=1;
+    async() => {
+        const browser = await puppeteer.launch({
+            headless: false
+        });
+        const page = await browser.newPage();
+        await page.goto('https://www.workana.com/en/freelancers/malaysia', {
+            waitUntil: 'load'
+        });
+        // await page.waitForSelector('.result_text a');
 
-while(count<3){
-    await page.click(`div > div.js-pager-container > div.text-center.hidden-xs.wk-pager-scroll > nav > ul > li:nth-child(${count}) > a`);
-    await autoScroll(page);
+        await page.setViewport({
+            width: 1200,
+            height: 800
+        });
 
-    data=await page.evaluate(() => {
+        //     const searchButtonNodeSelector = "div > div.js-pager-container > div.text-center.hidden-xs.wk-pager-scroll > nav > ul > li:nth-child(2) > a";
+        // await page.click(searchButtonNodeSelector);
+        var data;
+        var count = 1;
+        var ListName = [];
+        var ListHourlyRates = [];
+        var ListExperiences = [];
+        var ListCompletedProjects = [];
+        var ListCountries = [];
+        var ListRating = [];
 
-        var names = document.querySelectorAll("div > div > div.col-sm-7.col-md-8 > div.row > div.worker-details.col-xs-12.col-sm-7.col-md-9.text-center-xs > h3 > a");
-        var hourlyRates = document.querySelectorAll("div > div > div.col-sm-5.col-md-4.col-xs-12 > div.worker-details.text-center-sm > h4");
-        var experiences = document.querySelectorAll("div > div > div.col-sm-7.col-md-8 > div.skills.hidden-xs > div");
-        var completedProjects = document.querySelectorAll("div > div > div.col-sm-5.col-md-4.col-xs-12 > div.worker-details.text-center-sm > p:nth-child(2) > span:nth-child(1)");
-        var countries = document.querySelectorAll("div > div > div.col-sm-7.col-md-8 > div.row > div.worker-details.col-xs-12.col-sm-7.col-md-9.text-center-xs > span.country > span > a");
-        var ratings = document.querySelectorAll("div > div.col-sm-7.col-md-8 > div.row > div.worker-details.col-xs-12.col-sm-7.col-md-9.text-center-xs > label > span > span");
 
-        var name = Array.from(names, name => name.innerText);
-        var hourlyRate = Array.from(hourlyRates, hourlyRate => hourlyRate.innerText);
-        var experience =Array.from(experiences, experience => experience.innerText);
-        var completedProject = Array.from(completedProjects, completedProject => completedProject.innerText);
-        var country = Array.from(countries, country => country.innerText);
-        var rating = Array.from(ratings, rating => rating.innerText);
 
-        return {
-            name, hourlyRate, experience, completedProject, country, rating 
-        };
-    });
+        while (count < 6) {
 
-  count++;
+            await page.click(`div > div.js-pager-container > div.text-center.hidden-xs.wk-pager-scroll > nav > ul > li:nth-child(${count}) > a`);
+            await autoScroll(page);
 
-}
+            data = await page.evaluate((ListName) => {
 
-console.log(data.name);
-console.log(data.hourlyRate);
-console.log(data.experience);
-console.log(data.completedProject);
-console.log(data.country);
-console.log(data.rating);
+                var names = document.querySelectorAll("div > div > div.col-sm-7.col-md-8 > div.row > div.worker-details.col-xs-12.col-sm-7.col-md-9.text-center-xs > h3 > a");
+                var hourlyRates = document.querySelectorAll("div > div > div.col-sm-5.col-md-4.col-xs-12 > div.worker-details.text-center-sm > h4");
+                var experiences = document.querySelectorAll("div > div > div.col-sm-7.col-md-8 > div.skills.hidden-xs > div");
+                var completedProjects = document.querySelectorAll("div > div > div.col-sm-5.col-md-4.col-xs-12 > div.worker-details.text-center-sm > p:nth-child(2) > span:nth-child(1)");
+                var countries = document.querySelectorAll("div > div > div.col-sm-7.col-md-8 > div.row > div.worker-details.col-xs-12.col-sm-7.col-md-9.text-center-xs > span.country > span > a");
+                var ratings = document.querySelectorAll("div > div.col-sm-7.col-md-8 > div.row > div.worker-details.col-xs-12.col-sm-7.col-md-9.text-center-xs > label > span > span");
 
-// "#workers > div > div.js-pager-container > div.text-center.hidden-xs.wk-pager-scroll > nav > ul > li:nth-child(2) > a"
-// await page.$('#workers > div > div.js-pager-container > div.text-center.hidden-xs.wk-pager-scroll > nav > ul > li:nth-child(2) > a');
-0// Get the "viewport" of the page, as reported by the page.
-    
 
-    // for (i = 0; i < a.length; i++) {
-    //     var sql = "INSERT INTO job (name, company) VALUES ('" + a[i] + "'," + "'" + b[i] + "')";
-    //     con.query(sql, function (err, result) {
-    //         if (err) throw err;
-    //         console.log("All jobs record inserted");
-    //     });
-    // }
+                var name = Array.from(names, name => name.innerText);
+                var hourlyRate = Array.from(hourlyRates, hourlyRate => hourlyRate.innerText);
+                var experience = Array.from(experiences, experience => experience.innerText);
+                var completedProject = Array.from(completedProjects, completedProject => completedProject.innerText);
+                var country = Array.from(countries, country => country.innerText);
+                var rating = Array.from(ratings, rating => rating.innerText);
 
-    await browser.close();
 
-})();
+                // for (var i = 0; i < name.length; i++) {
+                //     ListName.push(name[i]);
+                // }
+                return {
+                    name,
+                    hourlyRate,
+                    experience,
+                    completedProject,
+                    country,
+                    rating
+                };
+            });
+
+            count++;
+
+            for (var i = 0; i < data.name.length; i++) {
+                ListName.push(data.name[i]);
+                ListHourlyRates.push(data.hourlyRate[i]);
+                ListExperiences.push(data.experience[i]);
+                ListCompletedProjects.push(data.completedProject[i]);
+                ListCountries.push(data.country[i]);
+                ListRating.push(data.rating[i]);
+
+            }
+        }
+
+
+
+
+        console.log(ListName);
+        console.log(ListHourlyRates);
+        console.log(ListExperiences);
+        console.log(ListCompletedProjects);
+        console.log(ListCountries);
+        console.log(ListRating);
+
+        // "#workers > div > div.js-pager-container > div.text-center.hidden-xs.wk-pager-scroll > nav > ul > li:nth-child(2) > a"
+        // await page.$('#workers > div > div.js-pager-container > div.text-center.hidden-xs.wk-pager-scroll > nav > ul > li:nth-child(2) > a');
+        // Get the "viewport" of the page, as reported by the page.
+
+
+        // for (i = 0; i < a.length; i++) {
+        //     var sql = "INSERT INTO job (name, company) VALUES ('" + a[i] + "'," + "'" + b[i] + "')";
+        //     con.query(sql, function (err, result) {
+        //         if (err) throw err;
+        //         console.log("All jobs record inserted");
+        //     });
+        // }
+
+        await browser.close();
+
+    })();
 
 async function autoScroll(page) {
-    await page.evaluate(async () => {
+    await page.evaluate(async() => {
         await new Promise((resolve, reject) => {
             var totalHeight = 0;
             var distance = 100;
